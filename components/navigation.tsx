c4ff35baf624e2367844302e6447e4395cb536b3
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-05 11:22:36 +0900

    ssr csr 떡밥

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,11 +1,19 @@
+"use client"
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navigation(){
+    // 그냥 실행 시 use client를 작성하라는 에러가 발생함
+    // 왜일까용?
+    const path = usePathname();
+    console.log(path);
+    
     return (
         <nav>
             <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about-us">About</Link></li>
+                <li><Link href="/">Home</Link> {path === "/" ? "!!" : ""}</li>
+                <li><Link href="/about-us">About</Link>{path === "/about-us" ? "!!" : ""}</li>
             </ul>
         </nav>
     );
