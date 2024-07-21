8332c3f679b75078ded742ed995f6f807a0c188d
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-21 15:52:34 +0900

    Loading component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,26 +1,22 @@
-"use client"
+export const metadata = {
+    title: "Home",
+}
 
-import { useEffect, useState } from "react";
 
-// export const metadata = {
-//     title: "Home",
-// }
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-    const [isLoading, setIsLoading] = useState(true); //fetch loading
-    const[movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-        const json = await response.json();
-        setMovies(json);
-        setIsLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, [])
+async function getMovies(){
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
     return (
         <div>
-            {isLoading ? "Loading..." : JSON.stringify(movies)}
+            {JSON.stringify(movies)}
         </div>
     );
 }
