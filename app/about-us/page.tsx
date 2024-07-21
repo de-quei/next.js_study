1a0dfc2e71e19cc1bb5aa5e051244b4bf1cab3ce
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-21 12:59:55 +0900

    layout 이용하기!

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "../../components/navigation";
 export default function Page() {
     return (
         <div>
-            <Navigation/>
             <h1>About us!</h1>
         </div>
     );
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,11 +5,7 @@ import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Navigation(){
-    // 그냥 실행 시 use client를 작성하라는 에러가 발생함
-    // 왜일까용?
     const path = usePathname();
-    const [count, setCount] = useState(0);
-
     console.log(path);
     
     return (
@@ -17,7 +13,6 @@ export default function Navigation(){
             <ul>
                 <li><Link href="/">Home</Link> {path === "/" ? "!!" : ""}</li>
                 <li><Link href="/about-us">About</Link>{path === "/about-us" ? "!!" : ""}</li>
-                <li><button onClick={() => setCount(c => c+1)}>{count}</button></li>
             </ul>
         </nav>
     );
