464faa5144d61c83252c3fa804e8cd89633b4648
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-21 13:41:44 +0900

    layout 이름변경 안햇다...

--- app/about-us/company/jobs/sales/layout.tsx
+++ app/about-us/company/jobs/sales/layout.tsx
@@ -1,4 +1,4 @@
-export default function RootLayout({
+export default function SalesLayout({
     children,
   }: {
     children: React.ReactNode
--- app/about-us/layout.tsx
+++ app/about-us/layout.tsx
@@ -1,4 +1,4 @@
-export default function RootLayout({
+export default function AboutUsLayout({
   children,
 }: {
   children: React.ReactNode
