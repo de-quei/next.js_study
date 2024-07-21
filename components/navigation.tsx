017a2df1f9175d59fdf0fc3c3e660ab21d70e8ab
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-21 16:55:22 +0900

    css modules

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,14 +2,14 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation(){
     const path = usePathname();
     console.log(path);
     
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li><Link href="/">Home</Link> {path === "/" ? "!!" : ""}</li>
                 <li><Link href="/about-us">About</Link>{path === "/about-us" ? "!!" : ""}</li>
